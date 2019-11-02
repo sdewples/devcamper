d664a8f0cc01dfa3c75d555169984c6b98ceaa8c
daniel <sdewples@gmail.com>
2019-11-02 16:46:42 -0400

    added json files, middleware, and error handling

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors')
 const connectDB = require('./config/db')
 
 
@@ -16,6 +17,9 @@ const bootcamps = require('./routes/bootcamps');
 // Initialize express app
 const app = express();
 
+// Body Parser
+app.use(express.json());
+
 // logs using morgan module in development mode
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -28,12 +32,12 @@ const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT, 
-  console.log(`App running in ${process.env.NODE_ENV} mode listening on port ${process.env.PORT}!`)
+  console.log(`App running in ${process.env.NODE_ENV} mode listening on port ${process.env.PORT}!`.yellow.bold)
 );
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}`.red.underline.bold);
   
   // Close server and exit process if server fails
   server.close(() => process.exit(1));
