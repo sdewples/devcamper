15d71953338c882397218cad3a535a2ad0803b05
daniel <sdewples@gmail.com>
2019-11-02 17:55:08 -0400

    better error handling

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,4 +1,5 @@
 const Bootcamp = require('../models/Bootcamp');
+const ErrorResponse = require('../utils/errorResponse');
 
 // @desc      Get all bootcamps
 // @route     GET /api/v1/bootcamps
@@ -27,17 +28,13 @@ exports.getBootcamp = async (req, res, next) => {
     const bootcamp = await Bootcamp.findById(req.params.id);
 
     if(!bootcamp) {
-      res
-        .status(400)
-        .json({ success: false });
+      return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
     }
     res
       .status(200)
       .json({ success: true, data: bootcamp })
   } catch (err) {
-    // res
-    //   .status(400)
-    //   .json({ success: false });
+
     next(err);
   }
 };
@@ -57,9 +54,7 @@ exports.createBootcamp = async (req, res, next) => {
         data: bootcamp
       });
   } catch (err) {
-    res
-      .status(400)
-      .json({ success: false});
+    next(err);
   }
 
 };
@@ -75,9 +70,7 @@ exports.updateBootcamp = async (req, res, next) => {
     });
   
     if (!bootcamp) {
-      res
-        .status(400)
-        .json({ success: false});
+      return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
     }
   
     res
@@ -85,9 +78,7 @@ exports.updateBootcamp = async (req, res, next) => {
       .json({success: true, data: bootcamp});
     
   } catch (err) {
-    res
-      .status(400)
-      .json({ success: false});
+    next(err);
   }
 
 };
@@ -100,9 +91,7 @@ exports.deleteBootcamp = async (req, res, next) => {
     const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
   
     if (!bootcamp) {
-      res
-        .status(400)
-        .json({ success: false});
+      return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
     }
   
     res
@@ -110,8 +99,6 @@ exports.deleteBootcamp = async (req, res, next) => {
       .json({success: true, data: {} });
     
   } catch (err) {
-    res
-      .status(400)
-      .json({ success: false });
+    next(err);
   }
 };
